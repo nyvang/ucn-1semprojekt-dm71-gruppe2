37e42fa5d0e1121dfe817f192776db02d7acc222
anitalykkeclemmensen@gmail.com <anitalykkeclemmensen@gmail.com@0abc4beb-c72d-ebe4-d09e-c3f9298867fa>
2010-05-25 09:18:25 +0000

    ProductContainer er opdateret

--- Sale/src/ModelLag/ProductContainer.java
+++ Sale/src/ModelLag/ProductContainer.java
@@ -14,52 +14,52 @@ import java.util.ArrayList;
 public class ProductContainer
 {
     private ArrayList<ProductDescription> stockList;
-    private ArrayList<String> addressList;
-    private ArrayList<String> buildingList;
-    private String address;
-    private String building;
+//    private ArrayList<String> addressList;
+//    private ArrayList<String> buildingList;
+//    private String address;
+//    private String building;
 
     public ProductContainer( String address, String building) 
     {
         stockList = new ArrayList<ProductDescription>();
-        addressList = new ArrayList<String>();
-        buildingList = new ArrayList<String>();
-        this.address = address;
-        this.building = building;
+//        addressList = new ArrayList<String>();
+//        buildingList = new ArrayList<String>();
+//        this.address = address;
+//        this.building = building;
     }
 
     
     /**
      * @return the address
      */
-    public String getAddress()
-    {
-        return address;
-    }
+//    public String getAddress()
+//    {
+//        return address;
+//    }
 
     /**
      * @param address the address to set
      */
-    public void setAddress(String address)
-    {
-        this.address = address;
-    }
+//    public void setAddress(String address)
+//    {
+//        this.address = address;
+//    }
 
     /**
      * @return the building
      */
-    public String getBuilding()
-    {
-        return building;
-    }
+//    public String getBuilding()
+//    {
+//        return building;
+//    }
 
     /**
      * @param building the building to set
      */
-    public void setBuilding(String building)
-    {
-        this.building = building;
-    }
+//    public void setBuilding(String building)
+//    {
+//        this.building = building;
+//    }
 
     /**
      *@param find ProductDescription
